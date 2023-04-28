ac1446eb6c136057ef89153cd75f3d9d9e118fd2
hung <clucky0123@gmail.com>
2023-04-28 08:35:56 +0700

    first commit

---
+++ src/Rank.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Gameplay
+{
+    internal class Player
+    {
+        public int score;
+        public string name;
+        public Player(string info)
+        {
+            string[] infos = info.Split(',');
+            name = infos[0];
+            score = Int32.Parse(infos[1]);
+        }
+        public Player(string name, int score)
+        {
+            this.name = name;
+            this.score = score;
+        }
+        public override string ToString()
+        {
+            return name + ',' + score;
+        }
+    }
+
+    internal class Rank
+    {
+        private frmMain contain;        
+        private Player[] players = new Player[3];
+
+        public Rank(frmMain contain, int yourScore)
+        {
+            this.contain = contain;            
+
+            ReadPlayerInfo();
+            GetPlayerInfo(yourScore, GetRank(yourScore));
+        }                
+        
+        private void ReadPlayerInfo()
+        {
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);            
+            if (!File.Exists(path + @"\rank.csv"))
+            {
+                FileStream stream = File.Create(path + @"\rank.csv");
+                stream.Flush();
+                stream.Close();
+            }
+            using (StreamReader reader = new StreamReader(path + @"\rank.csv"))
+            {
+                string info;
+                for (int i = 0; i < players.Length && (info = reader.ReadLine()) != null; i++)                    
+                        players[i] = new Player(info);                    
+            }
+        }
+
+        private int GetRank(int score) {
+            //not ranked when score = 0
+            if (score == 0) return -1;
+
+            //determine rank
+            int rank = 0;
+            while(rank < players.Length && players[rank] != null)
+            {
+                if (score > players[rank].score)
+                    break;
+                else
+                    rank++;
+            }            
+            return rank;                
+        }
+
+        private void GetPlayerInfo(int score, int rank)
+        {
+
+            //player not get ranked
+            if (rank >= players.Length || rank < 0) Display();
+            else
+            {
+                //notify when player get ranked
+                MessageBox.Show($"Bạn đạt được top {rank + 1}\nHãy ghi tên mình vào trong textbox.", "Chúc mừng!", MessageBoxButtons.OK);
+
+                //get player name
+                TextBox nameEntry = new TextBox();
+                nameEntry.Size = new Size(250, 100);
+                nameEntry.Location = new Point((contain.Width - nameEntry.Width) / 2, (contain.Height - nameEntry.Height) / 2);
+                nameEntry.KeyDown += (object sender, KeyEventArgs arg) => {
+                    if (arg.KeyCode == Keys.Enter)
+                    {
+                        string name = (!string.IsNullOrEmpty(nameEntry.Text)) ? nameEntry.Text : "No name";
+                        contain.Controls.Remove(nameEntry);
+                        Update(name, score, rank);
+
+                    }
+                };
+                contain.Controls.Add(nameEntry);
+                contain.Invalidate();
+            }
+        }
+
+
+        private void Update(string name, int score, int rank) {
+            //order
+            Player player = new Player(name, score);            
+            for (int i = players.Length - 1; i > rank; i--) 
+                players[i] = players[i - 1];            
+            players[rank] = player;
+
+            //update data file
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            using (StreamWriter writer = new StreamWriter(path + @"\rank.csv")) {
+                for (int i = 0; i < players.Length && players[i] != null; i++)                                         
+                    writer.WriteLine(players[i]);                
+            }
+            
+            Display(true);
+        }
+
+        private void Display(bool isPlayerGetRanked = false)
+        {
+            string rankedList = "";
+            for(int i = 0; i < players.Length && players[i] != null; i++)
+                rankedList += (char)(65 + i) + " " + players[i].name + " - " + players[i].score + '\n';
+            if (!isPlayerGetRanked)
+                rankedList += "\nBẠN THUA RỒI!\n DO BẠN GÀ Á.";
+            else
+                rankedList += "\nCHÚC MỪNG BẠN ĐÃ ĐẠT TOP";
+
+                //show game overoption
+                onGameoverOption(rankedList);
+            contain.Invalidate();            
+        }
+
+        private void onGameoverOption(string rankedList)
+        {
+            //label
+            Label label = new Label();
+            label.Text = rankedList;
+            label.Size = new Size(500,500);
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            label.BackColor = Color.Transparent;
+            label.Font = new Font("Lucida Handwriting", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label.ForeColor = Color.Black;
+            label.Location = new Point((contain.Width - label.Width) / 2, (contain.Height - label.Height) / 2 - 70);
+
+            // btnReset
+            Button btnReset = new Button();
+            btnReset.BackColor = Color.Transparent;
+            btnReset.BackgroundImageLayout = ImageLayout.Stretch;
+            btnReset.FlatAppearance.BorderColor = Color.White;
+            btnReset.FlatAppearance.BorderSize = 0;
+            btnReset.FlatAppearance.MouseOverBackColor = Color.Tan;
+            btnReset.FlatStyle = FlatStyle.Flat;
+            btnReset.Font = new Font("Lucida Handwriting", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            btnReset.ForeColor = Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            btnReset.Location = new Point(40, 435);
+            btnReset.Margin = new Padding(2);
+            btnReset.Size = new Size(96, 54);
+            btnReset.Text = "Chơi lại";
+            btnReset.UseVisualStyleBackColor = false;
+            btnReset.Click += new EventHandler(this.btnReset_Click);
+
+            // btnExit
+            Button btnExit = new Button(); 
+            btnExit.BackColor = Color.Transparent;
+            btnExit.BackgroundImageLayout = ImageLayout.Stretch;            
+            btnExit.FlatAppearance.BorderColor = Color.White;
+            btnExit.FlatAppearance.BorderSize = 0;
+            btnExit.FlatAppearance.MouseOverBackColor = Color.Tan;
+            btnExit.FlatStyle = FlatStyle.Flat;
+            btnExit.Font = new Font("Lucida Handwriting", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            btnExit.ForeColor = Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            btnExit.Location = new Point(267, 435);
+            btnExit.Margin = new Padding(2);            
+            btnExit.Size = new Size(96, 54);            
+            btnExit.Text = "Thoát";
+            btnExit.UseVisualStyleBackColor = false;            
+            btnExit.Click += new EventHandler(this.btnExit_Click);
+
+            //add to contain
+            contain.Controls.AddRange(new Control[]{ btnReset, btnExit, label});
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            frmMain frm = new frmMain();
+            contain.Hide();
+            frm.ShowDialog();
+            contain.Close();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            frmWelcome frm = new frmWelcome();
+            contain.Hide();
+            frm.ShowDialog();
+            contain.Close();
+        }
+    }
+}
---
+++ src/frmWelcome.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Gameplay
+{
+    public partial class frmWelcome : Form
+    {
+        public frmWelcome()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            frmMain Main = new frmMain();
+            this.Hide();
+            Main.ShowDialog();
+            this.Close();
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Game Stick Hero\r\n" +
+                "Người viết: Nhóm TTHP\r\n","Thông tin", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Nhấn giữ phím Space để tạo cầu\r\n" +
+                "Giữ càng lâu cầu càng dài\r\n" +
+                "Cầu chỉ dài chứ không ngắn lại\r\n" +
+                "Cầu đặt được lên núi và qua cầu được 1 điểm\r\n" +
+                "Cầu đặt được lên núi ngay điểm màu đỏ và qua cầu được 3 điểm\r\n" +
+                "Cầu quá ngắn không đặt được lên núi hoặc đặt được lên núi " +
+                "nhưng cầu quá dài bạn sẽ rớt xuống vực và thua cuộc",
+                "Hướng dẫn", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.Transparent;
+            this.btnPlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPlay.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnPlay.FlatAppearance.BorderSize = 0;
+            this.btnPlay.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Tan;
+            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlay.Font = new System.Drawing.Font("Lucida Handwriting", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnPlay.Location = new System.Drawing.Point(184, 236);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(151, 58);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "BẮT ĐẦU";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.Font = new System.Drawing.Font("Nirmala UI", 45F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(148, 46);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(199, 162);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.Text = "STICK\r\nHERO";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.BackColor = System.Drawing.Color.Transparent;
+            this.btnAbout.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnAbout.FlatAppearance.BorderSize = 0;
+            this.btnAbout.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Tan;
+            this.btnAbout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAbout.Font = new System.Drawing.Font("Lucida Handwriting", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbout.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnAbout.Location = new System.Drawing.Point(170, 308);
+            this.btnAbout.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(172, 58);
+            this.btnAbout.TabIndex = 2;
+            this.btnAbout.Text = "THÔNG TIN";
+            this.btnAbout.UseVisualStyleBackColor = false;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.BackColor = System.Drawing.Color.Transparent;
+            this.btnHelp.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnHelp.FlatAppearance.BorderSize = 0;
+            this.btnHelp.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Tan;
+            this.btnHelp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHelp.Font = new System.Drawing.Font("Lucida Handwriting", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHelp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnHelp.Location = new System.Drawing.Point(170, 370);
+            this.btnHelp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(182, 58);
+            this.btnHelp.TabIndex = 3;
+            this.btnHelp.Text = "HƯỚNG DẪN";
+            this.btnHelp.UseVisualStyleBackColor = false;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // frmWelcome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Gameplay.Properties.Resources.Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(481, 484);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.lblTitle);
+            this.Icon = global::Gameplay.Properties.Resources.CharacterICO;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.Name = "frmWelcome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Stick hero";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.Button btnHelp;
+    }
+}
